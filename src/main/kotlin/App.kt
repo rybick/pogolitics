408c2ba6fbc84b1501fea54d21b153979ba8cded
Arsen <bzareba21@gmail.com>
2020-05-16 22:21:31 +0200

    Upgrades to the table
    + change project name

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,16 +5,22 @@ class App: RComponent<RProps, RState>() {
         child(MovesetsTable::class) {
             attrs.values = listOf(
                 Moveset(
+                    fastAttack = Attack(PokemonType.STEEL, "Metal Claw"),
+                    chargedAttack = Attack(PokemonType.ICE, "Blizzard"),
+                    dps = 13.245F,
+                    timeToFirstAttack = 5.00F
+                ),
+                Moveset(
                     fastAttack = Attack(PokemonType.PSYCHIC, "Confuse"),
                     chargedAttack = Attack(PokemonType.PSYCHIC, "Psystrike"),
-                    dps = 13.245F,
+                    dps = 12.345F,
                     timeToFirstAttack = 8.00F
                 ),
                 Moveset(
-                    fastAttack = Attack(PokemonType.STEEL, "Metal Claw"),
-                    chargedAttack = Attack(PokemonType.ICE, "Blizzard"),
-                    dps = 12.345F,
-                    timeToFirstAttack = 5.00F
+                    fastAttack = Attack(PokemonType.NORMAL, "Yawn"),
+                    chargedAttack = Attack(PokemonType.ROCK, "Ancient Power"),
+                    dps = 2.345F,
+                    timeToFirstAttack = 5.50F
                 )
             )
         }
--- src/main/kotlin/MovesetsTable.kt
+++ src/main/kotlin/MovesetsTable.kt
@@ -1,4 +1,6 @@
 import kotlinx.css.*
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.unsafe
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -7,8 +9,18 @@ import styled.StyleSheet
 import styled.css
 import styled.styledDiv
 import kotlin.Float
+import react.*
 
-class MovesetsTable: RComponent<MovesetsRProps, RState>() {
+class MovesetsTable(props: MovesetsRProps) : RComponent<MovesetsRProps, MovesetsRState>(props) {
+    init {
+        state.values = props.values
+        state.sort = null
+    }
+
+    private val UP_ICON = "&#9653;"
+    private val DOWN_ICON = "&#9663;"
+    private val SPACE = "&nbsp;"
+
     override fun RBuilder.render() {
         styledDiv {
             css { +Styles.table }
@@ -20,20 +32,51 @@ class MovesetsTable: RComponent<MovesetsRProps, RState>() {
                 styledDiv {
                     css {
                         +Styles.cell
+                        +Styles.headerCell
                         +Styles.first
                     }
                     +"Moveset"
                 }
                 styledDiv {
-                    css { +Styles.cell }
-                    +"DPS"
+                    css {
+                        +Styles.cell
+                        +Styles.headerCell
+                    }
+                    attrs {
+                        onClickFunction = {
+                            setState {
+                                val sort = this.sort
+                                this.sort = Sort(
+                                        columnId = 1,
+                                        ascending = if (sort?.columnId == 1) !sort.ascending else false
+                                )
+                                values = values.sortedBy { -it.dps * (sort?.ascendFactor ?: 1) }
+                            }
+                        }
+                        unsafe { + ("DPS" + getIcon(state.sort, 1)) }
+                    }
                 }
                 styledDiv {
-                    css { +Styles.cell }
-                    +"TTFA"
+                    css {
+                        +Styles.cell
+                        +Styles.headerCell
+                    }
+                    attrs {
+                        onClickFunction = {
+                            setState {
+                                val sort = this.sort
+                                this.sort = Sort(
+                                    columnId = 2,
+                                    ascending = if (sort?.columnId == 2) !sort.ascending else false
+                                )
+                                values = values.sortedBy { -it.timeToFirstAttack * (sort?.ascendFactor ?: 1)  }
+                            }
+                        }
+                        unsafe { +("TTFA" + getIcon(state.sort, 2)) }
+                    }
                 }
             }
-            props.values.forEach {
+            state.values.forEach {
                 styledDiv {
                     css { +Styles.row }
                     styledDiv {
@@ -58,6 +101,14 @@ class MovesetsTable: RComponent<MovesetsRProps, RState>() {
         }
     }
 
+    private fun getIcon(sort: Sort?, columnId: Int): Any? {
+        if (sort != null && sort.columnId == columnId) {
+            return if (sort.ascending) UP_ICON else DOWN_ICON
+        } else {
+            return SPACE
+        }
+    }
+
     private object Styles: StyleSheet("ComponentStyles", isStatic = true) {
         val cell by css {
             display = Display.tableCell
@@ -68,6 +119,10 @@ class MovesetsTable: RComponent<MovesetsRProps, RState>() {
             borderLeftWidth = LinearDimension("1px")
             borderTopWidth = LinearDimension("0px")
             borderRightWidth = LinearDimension("0px")
+        }
+
+        val headerCell by css {
+            cursor = Cursor.pointer
         }
 
         val row by css {
@@ -94,6 +149,15 @@ class MovesetsTable: RComponent<MovesetsRProps, RState>() {
 
 external interface MovesetsRProps: RProps {
     var values: List<Moveset>
+}
+
+external interface MovesetsRState: RState {
+    var values: List<Moveset>
+    var sort: Sort?
+}
+
+data class Sort(val columnId: Int, val ascending: Boolean) {
+    val ascendFactor get() = if (ascending) 1 else -1
 }
 
 data class Moveset(
