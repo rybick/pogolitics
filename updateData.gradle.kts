0f47297f429f092774815ae57d33db59878bfb4b
bzareba <bzareba@pretius.com>
2020-05-24 01:07:44 +0200

    Tune up updateDate script (WIP)

--- updateData.gradle.kts
+++ updateData.gradle.kts
@@ -2,6 +2,7 @@ import java.net.URL
 import javax.json.Json
 import javax.json.JsonObject
 import javax.json.JsonArray
+import javax.json.JsonValue
 
 buildscript {
     repositories {
@@ -25,21 +26,38 @@ tasks.register("updateData") {
 }
 
 fun combinePokemonData(pokemonData: JsonObject, movesData: JsonArray): JsonObject {
-    val fastMoves: Set<JsonObject> = movesData.map { it.asJsonObject().getJsonObject("quickMove") }.toSet()
+    val quickMoves: Set<JsonObject> = movesData.map { it.asJsonObject().getJsonObject("quickMove") }.toSet()
     val chargedMoves: Set<JsonObject> = movesData.map { it.asJsonObject().getJsonObject("chargeMove") }.toSet()
-    val moves = Json.createObjectBuilder()
-        .add("quick", toJsonArray(fastMoves))
-        .add("charged", toJsonArray(chargedMoves))
-        .build()
     return Json.createObjectBuilder(pokemonData)
-        .add("moves", moves)
+        .add("moves", json("quick" to toJsonArray(quickMoves), "charged" to toJsonArray(chargedMoves)))
         .build()
 }
+
+fun json(vararg pairs: Pair<String, Any?>): JsonObject {
+    val builder = Json.createObjectBuilder()
+    pairs.forEach {
+        when (it.second) {
+            is JsonValue -> builder.add(it.first, it.second as JsonValue)
+            is String -> builder.add(it.first, it.second as String)
+            is BigInteger -> builder.add(it.first, it.second as String)
+            is BigDecimal -> builder.add(it.first, it.second as String)
+            is Int -> builder.add(it.first, it.second as String)
+            is Long -> builder.add(it.first, it.second as String)
+            is Double -> builder.add(it.first, it.second as String)
+            is Boolean -> builder.add(it.first, it.second as String)
+            null -> builder.addNull(it.first)
+            else -> throw Exception("Unexpected type of value " + it)
+        }
+    }
+    return builder.build()
+}
+
+fun json(vararg jsons: JsonValue): JsonArray = toJsonArray(jsons.toList())
 
 fun parseJsonArray(s: String): JsonArray = Json.createReader(s.reader()).use { it.readArray() }
 fun parseJsonObject(s: String): JsonObject = Json.createReader(s.reader()).use { it.readObject() }
 
-fun toJsonArray(list: Collection<JsonObject>): JsonArray {
+fun toJsonArray(list: Collection<JsonValue>): JsonArray {
     val x = Json.createArrayBuilder()
     list.forEach { x.add(it) }
     return x.build()
