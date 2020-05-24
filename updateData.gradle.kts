122457e642ccc871fbda93cc5ec6f05050154525
bzareba <bzareba@pretius.com>
2020-05-24 23:11:41 +0200

    Explicit json structure in updateData script

--- updateData.gradle.kts
+++ updateData.gradle.kts
@@ -28,9 +28,28 @@ tasks.register("updateData") {
 fun combinePokemonData(pokemonData: JsonObject, movesData: JsonArray): JsonObject {
     val quickMoves: Set<JsonObject> = movesData.map { it.asJsonObject().getJsonObject("quickMove") }.toSet()
     val chargedMoves: Set<JsonObject> = movesData.map { it.asJsonObject().getJsonObject("chargeMove") }.toSet()
-    return Json.createObjectBuilder(pokemonData)
-        .add("moves", json("quick" to toJsonArray(quickMoves), "charged" to toJsonArray(chargedMoves)))
-        .build()
+    val pokemon = pokemonData
+    return json(
+        "id" to pokemon.getInt("id"),
+        "baseAttack" to pokemon.getInt("atk"),
+        "baseDefense" to pokemon.getInt("def"),
+        "baseStamina" to pokemon.getInt("sta"),
+        "types" to json("primary" to pokemon.getString("type1"), "secondary" to pokemon.getString("type2", null)),
+        "moves" to json(
+            "quick" to toJsonArray(quickMoves.map(::mapMove)),
+            "charged" to toJsonArray(chargedMoves.map(::mapMove))
+        )
+    )
+}
+
+fun mapMove(move: JsonObject): JsonObject {
+    return json(
+        "id" to move.getInt("id"),
+        "name" to move.getString("name"),
+        "type" to move.getString("type"),
+        "legacy" to (move.getInt("isLegacy") != 0),
+        "exclusive" to (move.getInt("isExclusive") != 0)
+    )
 }
 
 fun json(vararg pairs: Pair<String, Any?>): JsonObject {
@@ -39,22 +58,22 @@ fun json(vararg pairs: Pair<String, Any?>): JsonObject {
         when (it.second) {
             is JsonValue -> builder.add(it.first, it.second as JsonValue)
             is String -> builder.add(it.first, it.second as String)
-            is BigInteger -> builder.add(it.first, it.second as String)
-            is BigDecimal -> builder.add(it.first, it.second as String)
-            is Int -> builder.add(it.first, it.second as String)
-            is Long -> builder.add(it.first, it.second as String)
-            is Double -> builder.add(it.first, it.second as String)
-            is Boolean -> builder.add(it.first, it.second as String)
+            is BigInteger -> builder.add(it.first, it.second as BigInteger)
+            is BigDecimal -> builder.add(it.first, it.second as BigDecimal)
+            is Int -> builder.add(it.first, it.second as Int)
+            is Long -> builder.add(it.first, it.second as Long)
+            is Double -> builder.add(it.first, it.second as Double)
+            is Boolean -> builder.add(it.first, it.second as Boolean)
             null -> builder.addNull(it.first)
-            else -> throw Exception("Unexpected type of value " + it)
+            else -> throw Exception("Unexpected type of value: $it")
         }
     }
     return builder.build()
 }
 
 fun json(vararg jsons: JsonValue): JsonArray = toJsonArray(jsons.toList())
-
 fun parseJsonArray(s: String): JsonArray = Json.createReader(s.reader()).use { it.readArray() }
+
 fun parseJsonObject(s: String): JsonObject = Json.createReader(s.reader()).use { it.readObject() }
 
 fun toJsonArray(list: Collection<JsonValue>): JsonArray {
