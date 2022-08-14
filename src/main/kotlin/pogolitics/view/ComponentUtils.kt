5e765c72e5dca7f62aec1c6fe05fc45196c81806
bzareba <bzareba21@gmail.com>
2022-08-14 19:13:51 +0200

    Link pokemon list page from main page

--- src/main/kotlin/pogolitics/view/ComponentUtils.kt
+++ src/main/kotlin/pogolitics/view/ComponentUtils.kt
@@ -3,12 +3,13 @@ package pogolitics.view
 import pogolitics.applicationRoot
 import pogolitics.model.PokemonType
 
-fun iconPath(type: PokemonType): String {
-    return path("/img/icon/${type.displayName}.png")
-}
+fun iconPath(type: PokemonType): String =
+    path("/img/icon/${type.displayName}.png")
 
-fun pokemonPagePath(pokedexNumber: Int, form: String? = null): String {
-    return path("/#/pokemon/${pokedexNumber}" + (form?.let { "?form=$it" } ?: ""))
-}
+fun pokemonPagePath(pokedexNumber: Int, form: String? = null): String =
+    path("/#/pokemon/${pokedexNumber}" + (form?.let { "?form=$it" } ?: ""))
+
+fun pokemonListPagePath(): String =
+    path("#/pokemon")
 
 private fun path(url: String): String = applicationRoot + url
--- src/main/kotlin/pogolitics/view/MainPage.kt
+++ src/main/kotlin/pogolitics/view/MainPage.kt
@@ -4,10 +4,16 @@ import pogolitics.PageRProps
 import react.RBuilder
 import react.RComponent
 import react.RState
+import react.dom.a
 import react.dom.p
 
 class MainPage(props: PageRProps<Unit, Unit>) : RComponent<PageRProps<Unit, Unit>, RState>(props) {
     override fun RBuilder.render() {
         p { +"page under construction" }
+        p {
+            a(href = pokemonListPagePath()) {
+                +"go to list of pokemon"
+            }
+        }
     }
 }
