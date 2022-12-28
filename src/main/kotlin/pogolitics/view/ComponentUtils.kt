5312d5542eba2270d4a2a06e4c46b7d7bbc4b681
bzareba <bzareba21@gmail.com>
2022-12-28 14:21:54 +0100

    make a dedicated mechanism for page path
    so that we can easily create slugs later

--- src/main/kotlin/pogolitics/view/ComponentUtils.kt
+++ src/main/kotlin/pogolitics/view/ComponentUtils.kt
@@ -1,6 +1,5 @@
 package pogolitics.view
 
-import org.w3c.dom.url.URL
 import pogolitics.applicationRoot
 import pogolitics.model.BattleMode
 import pogolitics.model.PokemonType
@@ -12,10 +11,36 @@ fun logoPath(): String =
     path("/img/logo/120x70.png")
 
 fun pokemonPagePath(pokedexNumber: Int, form: String? = null, mode: BattleMode = BattleMode.default): String =
-    path("/#/pokemon/${pokedexNumber}", mapOf("form" to form, "mode" to mode.toString()))
+    pagePath(Page.POKEMON(pokedexNumber, form, mode))
 
-fun pokemonListPagePath(): String =
-    path("#/pokemon")
+fun pokemonListPagePath(): String = pagePath(Page.POKEMON_LIST)
+
+fun pagePath(page: Page): String {
+    fun rawPagePath(page: Page?): String =
+        if (page == null) {
+            ""
+        } else {
+            rawPagePath(page.parent) + "/" + page.pathSegment
+        }
+    return path(rawPagePath(page), page.urlParams)
+}
+
+sealed class Page(
+    val parent: Page?,
+    val prettyName: String,
+    val pathSegment: String,
+    val urlParams: Map<String, String?> = mapOf()
+) {
+    object HOME: Page(null, "⌂", "#")
+    object POKEMON_LIST: Page(HOME, "pokemon list", "pokemon")
+    class POKEMON(pokedexNumber: Int, pokemonForm: String?, mode: BattleMode, prettyName: String? = null):
+        Page(
+            parent = POKEMON_LIST,
+            prettyName = prettyName ?: "$pokedexNumber",
+            pathSegment = "$pokedexNumber",
+            urlParams = mapOf("form" to pokemonForm, "mode" to mode.toString())
+        )
+} // TODO later make pokemonForm value object
 
 private fun path(baseUrl: String, params: Map<String, String?> = mapOf()): String =
     applicationRoot + baseUrl +
