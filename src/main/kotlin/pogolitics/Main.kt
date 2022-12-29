82bdcd095d9c610bb71e2738542a0909985b5f7a
bzareba <bzareba21@gmail.com>
2022-12-29 16:43:53 +0100

    Use the new render method

--- src/main/kotlin/pogolitics/Main.kt
+++ src/main/kotlin/pogolitics/Main.kt
@@ -1,8 +1,9 @@
 package pogolitics
 
-import react.dom.*
+import dom.Element
 import kotlinx.browser.document
-import org.w3c.dom.Element
+import react.RProps
+import react.createElement
 import react.dom.client.createRoot
 
 fun main() {
@@ -10,11 +11,16 @@ fun main() {
     document.createElement("div")
         .apply { id = "root" }
         .also { document.body!!.appendChild(it) }
+        // don't know why the types don't match here.
+        // It's the same js class but ported two kotlin twice. Both libs are the newest version.
+        .let { it as Element }
         .also { element: Element ->
-            //createRoot(element as dom.Element)//.render(App::class)
-            render(element as dom.Element) {
-                child(App::class) {}
-            }
+            createRoot(element)
+                .render(
+                    createElement<RProps> {
+                        child(App::class) {}
+                    }
+                )
         }
 }
 
