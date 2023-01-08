c57db0109e5f6a01a6e94d85acd17081e40cc658
bzareba <bzareba21@gmail.com>
2023-01-08 19:43:20 +0100

    Make KeyCodes const

--- src/main/kotlin/pogolitics/KeyCodes.kt
+++ src/main/kotlin/pogolitics/KeyCodes.kt
@@ -1,8 +1,8 @@
 package pogolitics
 
 object KeyCodes {
-    val enter = "Enter"
-    val numpadEnter = "NumpadEnter"
-    val arrowUp = "ArrowUp"
-    val arrowDown = "ArrowDown"
+    const val enter = "Enter"
+    const val numpadEnter = "NumpadEnter"
+    const val arrowUp = "ArrowUp"
+    const val arrowDown = "ArrowDown"
 }
