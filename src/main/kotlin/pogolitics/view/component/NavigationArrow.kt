9724f8c93ed5c23c8c740e17931718aa01a40a6e
bzareba <bzareba21@gmail.com>
2023-01-09 22:17:40 +0100

    Change nivigation arrow to use svg instead of a character

--- src/main/kotlin/pogolitics/view/component/NavigationArrow.kt
+++ src/main/kotlin/pogolitics/view/component/NavigationArrow.kt
@@ -1,54 +1,69 @@
 package pogolitics.view.component
 
-import csstype.ClassName
-import csstype.Content
 import csstype.Display
-import csstype.FontFamily
 import csstype.None
+import csstype.Position
 import csstype.TextAlign
-import csstype.deg
 import csstype.important
 import csstype.pct
 import csstype.px
-import csstype.rotate
-import csstype.scale
 import emotion.css.ClassName
 import emotion.react.css
-import pogolitics.arial
 import pogolitics.pointer
 import pogolitics.view.StyleConstants
+import react.ChildrenBuilder
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.a
 import react.dom.html.ReactHTML.span
+import react.dom.svg.ReactSVG.polygon
+import react.dom.svg.ReactSVG.svg
+import kotlin.math.sqrt
 
 val NavigationArrow = FC<ArrowProps> { props ->
     a {
         href = props.href
-        css(props.direction.style) {}
-        span {}
+        css(NavigationArrowStyles.arrow) {}
+        span {
+            svgTriangle(20.0, props.direction)
+        }
     }
 }
+
+fun ChildrenBuilder.svgTriangle(size: Double, direction: NavigationDirection) {
+    val middle = size / 2
+    val (pointingX, tailX) = when (direction) {
+        NavigationDirection.NEXT -> Pair(size, 0.25*size)
+        NavigationDirection.PREVIOUS -> Pair(0.0, 0.75*size)
+    }
+    svg {
+        width = size
+        height = size
+        polygon {
+            points = "$pointingX,$middle $tailX,${(2 + sqrt3) * size / 4} $tailX,${(2 - sqrt3) * size / 4}"
+            style = js("{\"fill\":\"white\"}") // TODO it uses hardcoded white, because css {} does not support fill
+        }
+    }
+}
+
+private val sqrt3 = sqrt(3.0)
 
 interface ArrowProps: Props {
     var direction: NavigationDirection
     var href: String
 }
 
-enum class NavigationDirection(val style: ClassName) {
-    PREVIOUS(NavigationArrowStyles.arrowPrevious), NEXT(NavigationArrowStyles.arrowNext);
+enum class NavigationDirection {
+    PREVIOUS, NEXT;
 }
 
 private object NavigationArrowStyles {
-    private val triangleUp = Char(9662).toString()
 
-    private val arrow = ClassName {
+    val arrow = ClassName {
         display = Display.inlineBlock
         cursor = pointer
         width = 42.px
         height = 42.px
-        fontFamily = arial
-        fontSize = 48.px
         textAlign = TextAlign.center
         borderRadius = 50.pct
         marginLeft = StyleConstants.Margin.small
@@ -60,29 +75,10 @@ private object NavigationArrowStyles {
             color = StyleConstants.Colors.primary.text
             textDecoration = None.none
         }
-    }
-
-    val arrowPrevious = ClassName(arrow) {
         span {
-            marginLeft = (-4).px
-            marginTop = (-15).px
-            display = Display.block
-            transform = rotate(90.deg)
-            after {
-                content = Content(triangleUp)
-            }
+            position = Position.relative
+            top = 1.px
         }
     }
 
-    val arrowNext = ClassName(arrow) {
-        span {
-            marginLeft = 4.px
-            marginTop = (-15).px
-            display = Display.block
-            transform = rotate((-90).deg)
-            after {
-                content = Content(triangleUp)
-            }
-        }
-    }
 }
